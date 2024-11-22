cdd3f06b5f71372bdf2afc66f25023ba1bfa62ad
Mia Taussig <miataussig@Mias-MacBook-Air.local>
2024-11-21 21:51:30 -0300

    probando/poniendo el apis para la pagina recetas

---
+++ js/receta.js
@@ -0,0 +1,30 @@
+const recetas = document.querySelector('.recetitas') 
+let recetin = ''  
+
+fetch('https://dummyjson.com/recipes')
+    .then(function(response){
+        return response.json();
+})
+    .then(function(data){
+        console.log(data.recipes)
+
+        for (let i=0; i < data.recipes.length; i++){
+            let right = data.recipes[i];
+            
+            let markUp = `
+                <article class="right">
+                    <img src= ${right.image} >
+                    <p> Name: ${right.name} </p>
+                    <p> Ingridients: ${right.ingredients} </p>
+                    <p> Instructions: ${right.instructions} </p>
+                </article>
+                `;
+            recetin += markUp;
+        }
+        recetas.innerHTML = recetin;
+        })
+        .catch(function (error){
+            console.log("Mi error fue", error);
+        })
+
+
