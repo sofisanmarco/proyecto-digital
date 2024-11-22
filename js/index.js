d44bc0e5d93e82fb6cb5687408b9c745bb454332
Mia Taussig <miataussig@Mias-MacBook-Air.local>
2024-11-22 09:47:49 -0300

    Integrando apis

--- js/index.js
+++ js/index.js
@@ -1,4 +1,28 @@
-<<<<<<< HEAD
+const recetas = document.querySelector('.recetitas') 
+let recetin = ''  
 
-=======
->>>>>>> c1715d547a9eeda9938eeb448e53a1d7a945bee7
+fetch('https://dummyjson.com/recipes')
+    .then(function(response){
+        return response.json();
+})
+    .then(function(data){
+        console.log(data.recipes)
+
+        for (let i=0; i < data.recipes.length; i++){
+            let right = data.recipes[i];
+            
+            let markUp = `
+                <article class="right">
+                    <img src= ${right.image} >
+                    <p> Name: ${right.name} </p>
+                    <p> Ingridients: ${right.ingredients} </p>
+                    <p> Instructions: ${right.instructions} </p>
+                </article>
+                `;
+            recetin += markUp;
+        }
+        recetas.innerHTML = recetin;
+        })
+        .catch(function (error){
+            console.log("Mi error fue", error);
+        })
