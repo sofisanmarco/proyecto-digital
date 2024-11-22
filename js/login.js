bdcf9f8069437647e8730b282ba7eab4812cfa12
sofisanma <ssanmarco@udesa.edu.ar>
2024-11-22 12:49:56 -0300

    empezado el login

---
+++ js/login.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("form")
+
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+
+const Invalidemail = document.querySelector(".email")
+const Invalidpassword = document.querySelector(".password")
+
+let errors = false
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    if (email.value === "") {
+        Invalidemail.innerText = "Incompleto"
+        Invalidemail.style.display = "block"
+        errors = true
+    } else {
+        Invalidemail.style.display = 'none'
+    }
+
+    if (password.value === "" || password.value.length < 3) {
+        Invalidpassword.innerText = "Incompleto"
+        Invalidpassword.style.display = 'block'
+        errors = true
+    } else {
+        Invalidpassword.style.display = 'none'
+    }
+
+    if (!errors) {
+        this.submit
+    }
+})
