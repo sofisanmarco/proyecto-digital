e5c94e6c4f473586209c0e802039e4b8ba6bf399
Mia Taussig <miataussig@Mias-MacBook-Air.local>
2024-11-23 02:28:27 -0300

    Use eventos en categories para que el texto se ponga mas grande con un mouseover. Despues intente cambiarle el color a rojo con el mouseover pero por ahora no se por que no funciona

--- js/categories.js
+++ js/categories.js
@@ -7,18 +7,53 @@ fetch('https://dummyjson.com/recipes/tags')
 })
     .then(function(data){
         console.log(data)
+        
         for (let i = 0; i < data.length; i++) {
             let tag = data[i]; 
             let markUp = `
                 <article class="cat">
-                 <p><a class="categoriescolor" href="category.html?id=${tag}">${tag}</a></p>
+                 <p class="veinte"><a class="categoriescolor" href="category.html?id=${tag}">${tag}</a></p>
                 </article>
                 `;
             type += markUp;   
         categorias.innerHTML = type;
+        const ratonarriba = document.querySelectorAll(`.veinte`);
+        for (let i =0; i<ratonarriba.length; i++){
+            let raton =ratonarriba[i]
+            raton.addEventListener('mouseover', function(){
+                raton.style.fontSize = '30px';
+    
+            
+            });
+            let ratin =ratonarriba[i]
+            ratin.addEventListener('mouseout', function(){
+                ratin.style.fontSize = '';
+                
+        
+            });
+
+       // const colores = document.querySelectorAll('.categoriescolor')
+       // for (let i=0; i<colores.length; i++){
+          //  let color = colores[i]
+          //  color.addEventListener('mouseover', function(){
+                //color.style.color = '#ae2012';
+
+          //  });
+           // let nocolor = colores[i]
+          //  nocolor.addEventListener('mouseover', function(){
+               // nocolor.style.color = '';
+
+         //  });
+
+       ///}
+
+            
+        }
+          
         }})
         .catch(function (error){
             console.log("Mi error fue", error);
         })
 
-    
+      
+        
